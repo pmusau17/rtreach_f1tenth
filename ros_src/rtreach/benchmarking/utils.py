f2a9eaed4d75cd4a3ef95c1c4721bf94e8182dd5
Patrick Musau <pmusau13ster@gmail.com>
2020-10-26 11:29:53 -0500

    printing changes

--- ros_src/rtreach/benchmarking/utils.py
+++ ros_src/rtreach/benchmarking/utils.py
@@ -7,6 +7,6 @@ def summarize_data(pd,sep_str="\n===============================================
     mean_experiment_time = sum_times.mean()
     mean_lec_percentage =  lec_percentage.mean()
     mean_safety_controller_percentage = safety_controller_percentage.mean()
-    print("\nlec usage %:",round(mean_lec_percentage,4)," | safety_controller usage %:",round(mean_safety_controller_percentage,4),"| moet: ",round(moet.values[0],4),"| mean_et:",round(meanet.values[0],4),sep_str)
+    print("\nlec usage %:",round(100*round(mean_lec_percentage,4),2)," | safety_controller usage %:",round(100*round(mean_safety_controller_percentage,4),2),"| moet: ",round(1000* round(moet.values[0],4),2),"| mean_et:",round(1000*round(meanet.values[0],4),2),sep_str)
     
     
